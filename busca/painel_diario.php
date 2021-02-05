9b6f84ec72fa519c428ac6283b54e3a692266e5e
branncko <imprimarte@gmail.com>
2021-02-05 13:28:14 -0300

    Index com refresh

--- busca/painel_diario.php
+++ busca/painel_diario.php
@@ -3,6 +3,8 @@
 
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Painel covid19 - CRUZ-CE</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
         integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
@@ -34,7 +36,7 @@
     }
     </style>
 
-    <title>Painel covid19 - CRUZ-CE</title>
+   
 </head>
 
 <body>
