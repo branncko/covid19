0b7d9b155225075e3eb10f94dcc03231a8b2e1bf
branncko <imprimarte@gmail.com>
2021-02-10 08:52:28 -0300

    Card Painel

---
+++ card-painel.php
@@ -0,0 +1,55 @@
+<div class="card">
+    <div class="card-body">
+        <?php while($row_boletim = $con->fetch_array()) { ?>
+        <h4 class="card-title">BOLETIM EPIDEMIOLÓGICO DO NOVO CORONAVÍRUS (COVID-19)</h4>
+        <p class="card-description"> <code> Última Atualização: <strong><?php echo $dia; ?></strong> às <strong><?php echo $atualiza[1]; ?></strong>
+                - Dados da Secretaria Municipal de Saúde (SESA).</code></p>
+        <div class="row">
+            <div class="col-lg-3 col-md-6">
+                <div class="d-flex">
+                    <div class="wrapper">
+                        <h2 class="font-weight-semibold mb-0"><?= $susp_total;?></h2>
+                        <div class="d-flex align-items-center pb-2">
+                            <div class="dot-indicator bg-warning mr-2"></div>
+                            <p class="mb-0">Casos Suspeitos</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 mt-md-0 mt-4">
+                <div class="d-flex">
+                    <div class="wrapper">
+                        <h2 class="font-weight-semibold mb-0"><?= $conf_total;?></h2>
+                        <div class="d-flex align-items-center pb-2">
+                            <div class="dot-indicator bg-danger mr-2"></div>
+                            <p class="mb-0">Casos Confirmados</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 mt-md-0 mt-4">
+                <div class="d-flex">
+                    <div class="wrapper">
+                        <h2 class="font-weight-semibold mb-0"><?= $descartados; ?></h2>
+                        <div class="d-flex align-items-center pb-2">
+                            <div class="dot-indicator bg-success mr-2"></div>
+                            <p class="mb-0">Casos Descartados</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 mt-md-0 mt-4">
+                <div class="d-flex">
+                    <div class="wrapper">
+                        <h2 class="font-weight-semibold mb-0"><?= $conf_obitos;?></h2>
+                        <div class="d-flex align-items-center pb-2">
+                            <div class="dot-indicator bg-dark mr-2"></div>
+                            <p class="mb-0">Óbitos</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php } ?>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php include 'conecta.php';
 include_once 'busca/variaveis.php';
+include_once 'includes/funcoes.php';
 	$consulta 		= "SELECT * FROM boletim ORDER BY dia DESC LIMIT 1";
 	$con      		= $mysqli_connection->query($consulta) or die($mysqli_connection->error);
 	$consulta_table = "SELECT * FROM boletim ORDER BY dia DESC LIMIT 7";
@@ -237,61 +238,7 @@ include_once 'busca/variaveis.php';
 					<div class="row">
 				
 						<div class="col-md-12 grid-margin stretch-card">
-							<div class="card">
-								<div class="card-body">
-									<?php while($row_boletim = $con->fetch_array()) { ?>
-									<h4 class="card-title">BOLETIM EPIDEMIOLÓGICO DO NOVO CORONAVÍRUS (COVID-19)</h4>
-									<p class="card-description"> <code> Última Atualização: <strong><?php echo $dia; ?></strong> às <strong><?php echo $atualiza[1]; ?></strong>
-                - Dados da Secretaria Municipal de Saúde (SESA).</code></p>
-									<div class="row">
-										<div class="col-lg-3 col-md-6">
-											<div class="d-flex">
-												<div class="wrapper">
-													<h2 class="font-weight-semibold mb-0"><?= $susp_total;?></h2>
-													<div class="d-flex align-items-center pb-2">
-														<div class="dot-indicator bg-warning mr-2"></div>
-														<p class="mb-0">Casos Suspeitos</p>
-													</div>
-												</div>
-											</div>
-										</div>
-										<div class="col-lg-3 col-md-6 mt-md-0 mt-4">
-											<div class="d-flex">
-												<div class="wrapper">
-													<h2 class="font-weight-semibold mb-0"><?= $conf_total;?></h2>
-													<div class="d-flex align-items-center pb-2">
-														<div class="dot-indicator bg-danger mr-2"></div>
-														<p class="mb-0">Casos Confirmados</p>
-													</div>
-												</div>
-											</div>
-										</div>
-										<div class="col-lg-3 col-md-6 mt-md-0 mt-4">
-											<div class="d-flex">
-												<div class="wrapper">
-													<h2 class="font-weight-semibold mb-0"><?= $descartados; ?></h2>
-													<div class="d-flex align-items-center pb-2">
-														<div class="dot-indicator bg-success mr-2"></div>
-														<p class="mb-0">Casos Descartados</p>
-													</div>
-												</div>
-											</div>
-										</div>
-										<div class="col-lg-3 col-md-6 mt-md-0 mt-4">
-											<div class="d-flex">
-												<div class="wrapper">
-													<h2 class="font-weight-semibold mb-0"><?= $conf_obitos;?></h2>
-													<div class="d-flex align-items-center pb-2">
-														<div class="dot-indicator bg-dark mr-2"></div>
-														<p class="mb-0">Óbitos</p>
-													</div>
-												</div>
-											</div>
-										</div>
-									</div>
-									<?php } ?>
-								</div>
-							</div>
+							<?php require_once "card-painel.php"; ?>
 						</div>
 					</div>
 					<div class="row">
