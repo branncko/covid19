6c1299957753918936eba2d859181d355b9a33f9
branncko <imprimarte@gmail.com>
2021-02-20 17:25:20 -0300

    Alterar atualizador

---
+++ admin/includes/salva_atualizador.php
@@ -0,0 +1,28 @@
+        <?php 
+        setlocale( LC_ALL, 'pt_BR', 'pt_BR.iso-8859-1', 'pt_BR.utf-8', 'portuguese' ); 
+        date_default_timezone_set( 'America/Sao_Paulo' );
+        //echo strftime( '%Y-%m-%d %H:%M:%S', strtotime('today') );
+        $hojeagora = strftime('%Y-%m-%d', strtotime('today') );
+        $datadiahoje = date('Y-m-d');
+        $datahorario = date('H:i');
+        echo $datadiahoje . "<br>"; 
+        echo $datahorario;
+        var_dump($hojeagora);
+
+        ?>
+
+        <?php 
+            $sql = "INSERT INTO boletim (dia, hora, suspeitos, classe_suspeitos, confirmados, classe_confirmados, descartados, obitos, classe_obitos) VALUES ('$datadiahoje' , '$datahorario', '$susp_total', '$classe_suspeitos', '$conf_total', '$classe_confirmados', '$descartados', '$conf_obitos', '$classe_obitos')";
+
+            if ($mysqli_connection->query($sql) === TRUE) {
+                echo  "Boletim incluído com sucesso!";
+            } else {
+                echo "Erro: " . $sql . "<br>" . $mysqli_connection->error;
+            }
+            
+            $mysqli_connection->close();
+            
+            
+            
+            
+        ?>
